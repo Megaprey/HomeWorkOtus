d38d36b90f2f0e81896b295819374cb2641407e2
Megaprey <Megaprey@outlook.com>
2020-04-25 21:03:18 +0300

    *hw03-annotation* исправил проблемы с проектом

---
+++ guava/hw01-gradle/src/main/java/ru/otus/homework/HelloOtus.java
@@ -0,0 +1,12 @@
+package ru.otus.homework;
+
+import com.google.common.collect.ArrayListMultimap;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        ArrayListMultimap<String, String> listMultimap = ArrayListMultimap.<String,String>create();
+        listMultimap.put("Синициин", "Воладимир");
+        listMultimap.put("Синициин", "Пётр");
+        System.out.println(listMultimap);
+    }
+}

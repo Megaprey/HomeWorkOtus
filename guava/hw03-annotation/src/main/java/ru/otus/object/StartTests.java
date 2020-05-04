526b8e5ce538bb891054069f88611f7f42084529
Megaprey <Megaprey@outlook.com>
2020-05-04 17:50:37 +0300

    *hw03-annotation* внёс исправления по замечаниям

--- guava/hw03-annotation/src/main/java/ru/otus/object/StartTests.java
+++ guava/hw03-annotation/src/main/java/ru/otus/object/StartTests.java
@@ -1,11 +1,13 @@
 package ru.otus.object;
 
+import ru.otus.annotation.After;
+import ru.otus.annotation.Before;
+import ru.otus.annotation.Test;
+
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 @SuppressWarnings("unchecked")
 public class StartTests {
@@ -15,6 +17,7 @@ public class StartTests {
             clazz = Class.forName(className);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+            return;
         }
 
         Method[] methodsPublic = clazz.getMethods();
@@ -24,36 +27,27 @@ public class StartTests {
 
         System.out.println("--- public methods:");
         for (Method method : methodsPublic) {
-            Annotation[] annotations = method.getDeclaredAnnotations();
-            addMethodWithAnnotation(afterMethods, method, "ru.otus.annotation.After", annotations);
-            addMethodWithAnnotation(beforeMethods, method, "ru.otus.annotation.Before", annotations);
-            addMethodWithAnnotation(testMethods, method, "ru.otus.annotation.Test", annotations);
+            addMethodWithAnnotation(afterMethods, method, After.class);
+            addMethodWithAnnotation(beforeMethods, method, Before.class);
+            addMethodWithAnnotation(testMethods, method, Test.class);
         }
         try {
-            runMethods(beforeMethods, clazz);
-            runMethods(testMethods, clazz);
-            runMethods(afterMethods, clazz);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
+            Object object = clazz.getConstructor().newInstance();
+            runMethods(beforeMethods, object);
+            runMethods(testMethods, object);
+            runMethods(afterMethods, object);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void addMethodWithAnnotation(ArrayList<Method> methodList, Method method, String annotationName, Annotation[] methAnnotations){
-        for (Annotation annotation : methAnnotations) {
-            if(annotation.annotationType().getName().equals(annotationName)){
-                methodList.add(method);
-            }
+    private void addMethodWithAnnotation(ArrayList<Method> methodList, Method method, Class annotationClass){
+        if (method.isAnnotationPresent(annotationClass)) {
+            methodList.add(method);
         }
     }
 
-    private void runMethods(ArrayList<Method> methods, Class clazz) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        Object object = clazz.getConstructor().newInstance();
+    private void runMethods(ArrayList<Method> methods, Object object) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         for (Method method: methods) {
             method.invoke(object);
         }

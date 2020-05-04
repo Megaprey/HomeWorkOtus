c27ff8ab08d367a771b25f942a8456c270f2d468
Megaprey <Megaprey@outlook.com>
2020-05-04 17:00:41 +0300

    *hw02-ArrayList* создал DIYarrayList работающий для трёх методов

--- guava/hw2-ArrayList/src/main/java/ru/otus/homework/DIYarrayList.java
+++ guava/hw2-ArrayList/src/main/java/ru/otus/homework/DIYarrayList.java
@@ -3,13 +3,28 @@ package ru.otus.homework;
 import java.util.*;
 
 public class DIYarrayList<T> implements List<T> {
-    private T mass[];
+    private T[] mass;
     private int size;
+    private int capacity = 16;
     public static void main(String[] args) {
         DIYarrayList<String> diYarrayList1 = new DIYarrayList<>();
         Collections.addAll(diYarrayList1,  "sadsa", "sadsa", "sadsa", "sadsa", "sadsa", "sadsa",
-                "sadsa", "sadsa", "sadsa", "sadsa", "sadsa", "sadsa", "sadsa", "sadsa", "sadsa", "sadsa",
-                "sadsa", "sadsa", "sadsa", "sadsa", "sadsa", "sadsa", "sadsa");
+                "sadsa", "sadsa", "sadsa", "sadsa", "abc", "sadsa", "sadsa", "sadsa", "sadsa", "sadsa",
+                "sadsa", "sadsa", "sadsa", "sadsa", "bca", "sadsa", "sadsa");
+        System.out.println(diYarrayList1);
+        DIYarrayList<String> diYarrayList2 = new DIYarrayList<>();
+        Collections.addAll(diYarrayList2,  "1", "2", "3", "4", "5", "6",
+                "7", "8", "9", "10", "11", "12", "13", "14", "15", "16",
+                "17", "18", "19", "20", "21", "22", "23");
+        Collections.copy(diYarrayList2, diYarrayList1);
+        System.out.println(diYarrayList2);
+        Collections.sort(diYarrayList2, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        System.out.println(diYarrayList2);
     }
 
     public DIYarrayList(){
@@ -46,12 +61,14 @@ public class DIYarrayList<T> implements List<T> {
 
     @Override
     public Iterator<T> iterator() {
-        return new Iter();
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Object[] toArray() {
-        return mass;
+        Object[] massResult = new Object[size];
+        System.arraycopy(mass,0, massResult, 0, size);
+        return massResult;
     }
 
     @Override
@@ -61,45 +78,53 @@ public class DIYarrayList<T> implements List<T> {
 
     @Override
     public boolean add(T t) {
-        mass[mass.length - 1] = t;
-
-        return false;
+        size++;
+        if(size > capacity){
+            capacity = capacity * 2;
+            T[] massTmp = (T[]) new Object[capacity];
+            for(int i = 0; i < mass.length; i++){
+                massTmp[i] = mass[i];
+            }
+            mass = massTmp;
+        }
+        mass[size - 1] = t;
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException();
     }
 
 
     @Override
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends T> c) {
-        return false;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public void clear() {
-
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -129,43 +154,92 @@ public class DIYarrayList<T> implements List<T> {
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public ListIterator<T> listIterator() {
-        return null;
+        return new ListIter() ;
     }
 
     @Override
     public ListIterator<T> listIterator(int index) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public List<T> subList(int fromIndex, int toIndex) {
-        return null;
+        throw new UnsupportedOperationException();
     }
-    private class Iter implements Iterator<T>{
+
+    private class ListIter implements ListIterator<T>{
+        int cursor;
+        int cursorForThisIter;
 
         @Override
         public boolean hasNext() {
-            return false;
+            return cursor != size;
         }
 
         @Override
         public T next() {
-            return null;
+            if (cursor >= size || cursor < 0) {
+                System.err.println("нет такого элемента");
+            }
+            cursorForThisIter = cursor;
+            cursor++;
+
+            return (T) DIYarrayList.this.mass[cursorForThisIter];
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public T previous() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int nextIndex() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int previousIndex() {
+            throw new UnsupportedOperationException();
         }
 
         @Override
         public void remove() {
+            throw new UnsupportedOperationException();
 
         }
+
+        @Override
+        public void set(T t) {
+            DIYarrayList.this.mass[cursorForThisIter] = t;
+        }
+
+        @Override
+        public void add(T t) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "DIYarrayList{" +
+                "mass=" + Arrays.toString(mass) +
+                ", size=" + size +
+                ", capacity=" + capacity +
+                '}';
     }
 }
